61fe1ef40feab7ddd0fb75ab3a4b89a6db115f86
Maria71k <149970790+Maria71k@users.noreply.github.com>
2024-03-03 09:22:29 +0100

    Add files via upload

---
+++ Sourced Counting.py
@@ -0,0 +1,7 @@
+def count_books_from_user_input():
+    num_books = int(input("Enter the number of books: "))
+    return num_books
+
+# Example usage:
+total_books = count_books_from_user_input()
+print("Total books from user input:", total_books)
